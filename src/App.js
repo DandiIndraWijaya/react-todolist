7934971719625e7d72abcc28c491f3616c39cd55
DandiIndraWijaya <dandiindra29@gmail.com>
2020-08-29 10:14:31 +0700

    Migrasi html dan css ke react

--- src/App.js
+++ src/App.js
@@ -4,22 +4,63 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+                        <div className="frame">
+                            <section className="header">
+                                <button className="header-btn main-black-color">
+                                    Add
+                                </button>
+                                <h1 className="header-title">
+                                    Todo Lists
+                                </h1>
+                                <button className="header-btn main-red-color">
+                                    Clear
+                                </button>
+                            </section>
+
+                            <section className="add">
+                                <form className="add-form">
+                                    <input type="text" className="add-input"></input>
+                                    <button className="add-btn main-black-color">
+                                        Add
+                                    </button>
+                                </form>
+                            </section>
+
+                            <section className="todos">
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                                <div className="todo">
+                                    <span className="todo-text">Learning React</span>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
   );
 }
 
