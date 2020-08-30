24aedf8917dd4cf8be17bee834ca35c48bb6c074
DandiIndraWijaya <dandiindra29@gmail.com>
2020-08-30 11:14:59 +0700

    Membuat addTodo di todolist page

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -6,8 +6,8 @@ const Todos = ({ todos }) => {
    
     return (
         <section className="todos">
-            {todos.map(todo => {
-               return <Todo text={todo.text} />
+            {todos.map((todo, index) => {
+               return <Todo key={index} text={todo.text} />
             })}                    
         </section>
     );
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -22,12 +22,18 @@ const TodoList = () => {
         {
             text: "Belajar React lur"
         },
-    ])
+    ]);
+
+    const addTodo = value => {
+        const addedTodo = [...todos, { text: value}];
+
+        setTodos(addedTodo);
+    };
 
   return ( 
     <Paper>
         <Header />
-        <TodoForm />
+        <TodoForm addTodo={addTodo} />
         <Todos todos={todos} />           
     </Paper>               
   );
